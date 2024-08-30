d8548b8e817fb50c56f5f236dfe97372181d5075
yunsik <144660222+yunsik123@users.noreply.github.com>
2024-08-30 12:17:51 +0900

    Add files via upload

---
+++ 10. 2차프로젝트/웹 디자인/web page/main.scripts.js
@@ -0,0 +1,4 @@
+// 메인 페이지의 AI 버튼 클릭 시 Select 페이지로 이동
+document.querySelector('.floating-button').addEventListener('click', function() {
+  window.location.href = 'select.html'; // Select 페이지로 이동
+});
